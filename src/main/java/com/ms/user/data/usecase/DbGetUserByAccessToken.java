70fc278114fa236e45fd9678795eb5191705b510
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-04 14:58:45 -0300

    feat: DbGetUserByAccessToken was created

---
+++ src/main/java/com/ms/user/data/usecase/DbGetUserByAccessToken.java
@@ -0,0 +1,17 @@
+package com.ms.user.data.usecase;
+
+import com.ms.user.data.protocol.GetUserByAccessTokenRepository;
+import com.ms.user.domain.modal.User;
+import com.ms.user.domain.usecase.GetUserByAccessToken;
+
+public class DbGetUserByAccessToken implements GetUserByAccessToken {
+    GetUserByAccessTokenRepository getUserByAccessTokenRepository;
+
+    public DbGetUserByAccessToken (GetUserByAccessTokenRepository repository) {
+        getUserByAccessTokenRepository = repository;
+    }
+
+    public User getByAccessToken(String accessToken) {
+        return getUserByAccessTokenRepository.getByAccessToken(accessToken);
+    }
+}
