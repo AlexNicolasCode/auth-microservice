812f99c79868be88f72101952b0b41b96960075f
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-04 15:50:21 -0300

    feat: user model was adpted to use spring boot jpa rules

--- src/main/java/com/ms/user/domain/modal/User.java
+++ src/main/java/com/ms/user/domain/modal/User.java
@@ -1,29 +1,45 @@
 package com.ms.user.domain.modal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="USER")
 public class User {
-    private Long id;    
+    @Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    
+    @NotNull
 	private String name;
-	private String email;
-	private Long createdAt;
-	private Long updatedAt;
+	
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getEmail() {
-        return email;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Long getCreatedAt() {
-        return createdAt;
-    }
-
-    public Long getUpdated() {
-        return updatedAt;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
