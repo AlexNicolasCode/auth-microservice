159bdd438d95ab3fc793a8d0e6e728c398bb888b
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-04 16:27:19 -0300

    feat: UserPostgresRepository was created with GetUserByAccessTokenRepository interface

---
+++ src/main/java/com/ms/user/infra/database/UserPostgresRepository.java
@@ -0,0 +1,15 @@
+package com.ms.user.infra.database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.ms.user.data.protocol.GetUserByAccessTokenRepository;
+import com.ms.user.domain.modal.User;
+
+@Repository
+public interface UserPostgresRepository extends JpaRepository<User, String>, GetUserByAccessTokenRepository {
+    @Override
+    @Query("SELECT * FROM USER u WHERE u.token = ?accessToken")
+    public User getByAccessToken(String accessToken);
+}
