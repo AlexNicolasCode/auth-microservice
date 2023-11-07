fe538288927e6ae3edaaa42544571ceaa1616ea6
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-07 13:27:33 -0300

    feat: DbUpdateToken should generate token internaly to enable UpdateTokenRepository works

--- src/main/java/com/ms/user/data/usecase/DbUpdateToken.java
+++ src/main/java/com/ms/user/data/usecase/DbUpdateToken.java
@@ -1,17 +1,21 @@
 package com.ms.user.data.usecase;
 
+import com.ms.user.data.protocol.GenerateToken;
 import com.ms.user.data.protocol.UpdateTokenRepository;
 import com.ms.user.domain.usecase.UpdateToken;
 
 public class DbUpdateToken implements UpdateToken {
-    public DbUpdateToken(UpdateTokenRepository updateTokenRepository) {
+    public DbUpdateToken(GenerateToken generateToken, UpdateTokenRepository updateTokenRepository) {
+        this.generateToken = generateToken;
         this.updateTokenRepository = updateTokenRepository;
     }
 
+    private GenerateToken generateToken;
     private UpdateTokenRepository updateTokenRepository;
 
     @Override
-    public String update(Long userId, String token) {
+    public String update(Long userId) {
+        String token = this.generateToken.generateToken(userId);
         return this.updateTokenRepository.updateToken(userId, token);
     }
 }
