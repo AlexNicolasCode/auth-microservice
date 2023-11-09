e2c6fd18b0f1cbe3f8b87d13087db089773dc812
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-09 19:09:56 -0300

    feat: change SendEmail send method params to enable receive subject and text

--- src/main/java/com/ms/user/data/protocol/SendEmail.java
+++ src/main/java/com/ms/user/data/protocol/SendEmail.java
@@ -1,5 +1,7 @@
-package com.ms.user.domain.usecase;
+package com.ms.user.data.protocol;
+
+import com.ms.user.domain.model.User;
 
 public interface SendEmail {
-    void send(Object params);
+    void send(User user, String subject, String text);
 }
