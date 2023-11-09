f602208634b5f729d2f6a3ad960302c2912390c7
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-09 19:23:40 -0300

    feat: RabbitMQProducer was created

---
+++ src/main/java/com/ms/user/infra/broker/RabbitMQProducer.java
@@ -0,0 +1,31 @@
+package com.ms.user.infra.broker;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.ms.user.data.protocol.SendEmail;
+import com.ms.user.domain.model.User;
+import com.ms.user.presentation.dto.EmailDto;
+
+@Component
+public class RabbitMQProducer implements SendEmail {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
+
+    public void send(User userModel, String subject, String text) {
+        EmailDto emailDto = new EmailDto();
+        emailDto.setUserId(userModel.getId());
+        emailDto.setEmailTo(userModel.getEmail());
+        emailDto.setSubject(subject);
+        emailDto.setText(userModel.getName() + text);
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
+}
