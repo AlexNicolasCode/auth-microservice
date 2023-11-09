5e3d707519c67a50fa060ea2f5f945d83f22dc3f
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-09 19:14:09 -0300

    feat: RemoteSendWelcomeEmail was created

---
+++ src/main/java/com/ms/user/data/usecase/RemoteSendWelcomeEmail.java
@@ -0,0 +1,18 @@
+package com.ms.user.data.usecase;
+
+import com.ms.user.data.protocol.SendEmail;
+import com.ms.user.domain.model.User;
+import com.ms.user.domain.usecase.SendWelcomeEmail;
+
+public class RemoteSendWelcomeEmail implements SendWelcomeEmail {
+    public RemoteSendWelcomeEmail(SendEmail sendEmail) {
+        this.sendEmail = sendEmail;
+    }
+
+    private SendEmail sendEmail;
+
+    @Override
+    public void sendEmail(User user) {
+        sendEmail.send(user, "Bem-vindo(A), " + user.getEmail(), "Testando!");
+    }
+}
