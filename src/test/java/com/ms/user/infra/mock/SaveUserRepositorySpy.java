a794d3fa5892afffaee0cf6df1be6f798386436f
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-10 15:00:07 -0300

    test: SaveUserRepository was mocked

---
+++ src/test/java/com/ms/user/infra/mock/SaveUserRepositorySpy.java
@@ -0,0 +1,22 @@
+package com.ms.user.infra.mock;
+
+import com.ms.user.data.protocol.SaveUserRepository;
+import com.ms.user.domain.model.User;
+
+
+public class SaveUserRepositorySpy implements SaveUserRepository {
+    
+    public int count = 0;
+    public User user;
+    public User result;
+
+    public User save(User user) {
+        this.count++;
+        this.user = user;
+        return user;
+    }
+
+    public User getUserParam() {
+        return this.user;
+    }
+}
