125bad3d2ff5632c608beb1b206ea8100d664e65
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-10 16:19:09 -0300

    test: DbUpdateToken should call GenerateToken with correct param

---
+++ src/test/java/com/ms/user/data/DbUpdateTokenTest.java
@@ -0,0 +1,26 @@
+package com.ms.user.data;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import com.ms.user.data.usecase.DbUpdateToken;
+import com.ms.user.domain.mock.UserMock;
+import com.ms.user.infra.mock.GenerateTokenSpy;
+import com.ms.user.infra.mock.UpdateTokenRepositorySpy;
+
+public class DbUpdateTokenTest {
+
+    @Test
+    public void shouldCallGenerateTokenWithCorrectParam() {
+        GenerateTokenSpy generateTokenSpy = new GenerateTokenSpy();
+        UpdateTokenRepositorySpy updateTokenRepositorySpy = new UpdateTokenRepositorySpy();
+        DbUpdateToken sut = new DbUpdateToken(generateTokenSpy, updateTokenRepositorySpy);
+        Long fakeUserId = new UserMock().build().getId();
+ 
+        sut.update(fakeUserId);
+
+        assertEquals(fakeUserId, generateTokenSpy.getUserId());
+    }
+
+}
