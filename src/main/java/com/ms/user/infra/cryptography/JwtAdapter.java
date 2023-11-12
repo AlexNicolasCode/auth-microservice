20c52bbb2e379d15ed1261b902c205cf321cbf64
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-12 15:11:33 -0300

    feat: apply new JWT sign to enable to improve token security

--- src/main/java/com/ms/user/infra/cryptography/JwtAdapter.java
+++ src/main/java/com/ms/user/infra/cryptography/JwtAdapter.java
@@ -1,25 +1,30 @@
 package com.ms.user.infra.cryptography; 
 
+import java.security.Key;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.ms.user.data.protocol.GenerateToken;
-
-import io.jsonwebtoken.Jwts;
 
 @Component
 public class JwtAdapter implements GenerateToken {
 
     @Override
-    public String generateToken(Long userId) {
+    public String generateToken(Long userId, Key publicKey, Key privateKey) {
         Long now = new Date().getTime();
         int sevenDays = 7 * 60 * 60 * 24 * 1000;
         Date serverDaysOnFuture = new Date(now + sevenDays);
-        String userIdConverted = userId.toString();
-        return Jwts.builder()
-            .claim("id", userIdConverted)
-            .expiration(serverDaysOnFuture)
-            .compact();
+        Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey);
+        String token = JWT.create()
+            .withIssuer("Bookue-api")
+            .withClaim("id", userId)
+            .withExpiresAt(serverDaysOnFuture)
+            .sign(algorithm);
+        return token;
     }
 }
