fff7e98ea7a2cf76df3a09ee9c10a2ec943bfd2f
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-12 15:24:20 -0300

    test: GenerateToken mock was updated to keep following GenerateToken interface

--- src/test/java/com/ms/user/infra/mock/GenerateTokenSpy.java
+++ src/test/java/com/ms/user/infra/mock/GenerateTokenSpy.java
@@ -1,5 +1,7 @@
 package com.ms.user.infra.mock;
 
+import java.security.Key;
+
 import com.github.javafaker.Faker;
 import com.ms.user.data.protocol.GenerateToken;
 
@@ -7,16 +9,28 @@ public class GenerateTokenSpy implements GenerateToken {
     
     private int count = 0;
     private Long userId;
+    private Key publicKey;
+    private Key privateKey;
     private String result = new Faker().internet().uuid();
 
-    public String generateToken(Long userId) {
+    public String generateToken(Long userId, Key publicKey, Key privateKey) {
         this.count++;
         this.userId = userId;
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
         return this.result;
     }
 
     public Long getUserId() {
         return this.userId;
+    }
+
+    public Key getPublicKey() {
+        return this.publicKey;
+    }
+
+    public Key getPrivateKey() {
+        return this.privateKey;
     }
 
     public String getResult() {
