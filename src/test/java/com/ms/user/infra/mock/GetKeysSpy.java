dc118a7d923df6443f06d79bd3feb36047dad81d
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-12 15:30:17 -0300

    test: add count on GetKey mock to enable test know how many times it class was called

--- src/test/java/com/ms/user/infra/mock/GetKeysSpy.java
+++ src/test/java/com/ms/user/infra/mock/GetKeysSpy.java
@@ -5,16 +5,23 @@ import java.security.Key;
 import com.ms.user.data.protocol.GetKeys;
 
 public class GetKeysSpy implements GetKeys {
+    private int count = 0;
     private Key privateKey;
     private Key publicKey;
 
     @Override
     public Key getPrivateKey() {
+        this.count++;
         return this.privateKey;
     }
-
+    
     @Override
     public Key getPublicKey() {
+        this.count++;
         return this.publicKey;
+    }
+
+    public int getCount () {
+        return this.count;
     }
 }
