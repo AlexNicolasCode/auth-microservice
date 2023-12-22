9ff5e0a5e3d74a1e8339e7f9bd707dba4aa132f5
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-22 14:57:12 -0300

    feat: remove unncessary files for User model to reduce complexity

--- src/main/java/com/ms/user/domain/model/User.java
+++ src/main/java/com/ms/user/domain/model/User.java
@@ -1,19 +1,9 @@
 package com.ms.user.domain.model;
 
 public class User {
-    private Long id;
 	private String name;
     private String email;
     private String password;
-    private String token;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return this.name;
@@ -37,13 +27,5 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getToken() {
-        return this.token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
     }
 }
