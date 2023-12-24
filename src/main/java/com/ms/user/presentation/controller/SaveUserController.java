726e752cbcfb0a14b7a4dd0981e635ad784b7e61
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 16:47:43 -0300

    feat: adapt body validation to enable a get a custom message that describe error from validation alerted by dto

--- src/main/java/com/ms/user/presentation/controller/SaveUserController.java
+++ src/main/java/com/ms/user/presentation/controller/SaveUserController.java
@@ -3,51 +3,43 @@ package com.ms.user.presentation.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ms.user.domain.model.User;
+import com.ms.user.domain.dto.SaveUserDto;
 import com.ms.user.domain.usecase.SaveUser;
 import com.ms.user.domain.usecase.SendWelcomeEmail;
 import com.ms.user.domain.usecase.UpdateToken;
-import com.ms.user.presentation.dto.SaveUserDto;
 import com.ms.user.presentation.protocol.Controller;
-import com.ms.user.presentation.protocol.Validator;
-
-import jakarta.validation.Valid;
 
 @RestController
-public class SaveUserController implements Controller<SaveUserDto, Errors, ResponseEntity<Object>> {
+public class SaveUserController implements Controller<SaveUserDto, ResponseEntity<Object>> {
 
 	public SaveUserController(
 		SaveUser saveUser,
 		UpdateToken updateToken,
-		Validator<Errors> validator,
 		SendWelcomeEmail sendWelcomeEmail
 	) {
 		this.saveUser = saveUser;
 		this.updateToken = updateToken;
-		this.validator = validator;
 		this.sendWelcomeEmail = sendWelcomeEmail;
 	}
 	
 	private SaveUser saveUser;
 	private UpdateToken updateToken;
-	private Validator<Errors> validator;
 	private SendWelcomeEmail sendWelcomeEmail;
     
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
-	public ResponseEntity<Object> handle(@Valid @RequestBody SaveUserDto userDto, Errors errors) {
+	public ResponseEntity<Object> handle(@RequestBody SaveUserDto userDto) {
 		Map<String, Object> response = new HashMap<String, Object>();
-		Object error = validator.validate(errors);
-		if (error != null) {
-			return new ResponseEntity<Object>(error, HttpStatus.BAD_REQUEST);
+		String errorMessage = userDto.getError();
+		if (errorMessage != null) {
+			response.put("error", errorMessage);
+			return new ResponseEntity<Object>(response, HttpStatus.BAD_REQUEST);
 		}
 		User user = new User();
 		BeanUtils.copyProperties(userDto, user);
