4b34ac1c29e81f87513e840d6553536ba206ad69
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 09:29:29 -0300

    feat: set Email model to avoid primitive obsession anti-pattern

---
+++ src/main/java/com/ms/user/domain/model/Email.java
@@ -0,0 +1,19 @@
+package com.ms.user.domain.model;
+
+import java.util.regex.Pattern;
+
+import com.ms.user.domain.exception.InvalidEmailException;
+
+public final class Email {
+    private String value;
+
+    public Email(String value) throws InvalidEmailException, Exception {
+        boolean isEmailFormat = Pattern.compile("^(.+)@(\\S+)$")
+            .matcher(value)
+            .matches();
+        if (!isEmailFormat){
+            throw new InvalidEmailException();
+        }
+        this.value = value;
+    }
+}
