ad0a2e7042b1413c6e7aeabd2d175d42cfc2c448
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 18:49:09 -0300

    feat: client side should be able to request authenticate user when request correctly POST /user/authentication

---
+++ src/main/java/com/ms/user/presentation/controller/AuthenticationController.java
@@ -0,0 +1,43 @@
+package com.ms.user.presentation.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.user.domain.dto.AuthenticationDto;
+import com.ms.user.domain.model.DefaultReturn;
+import com.ms.user.domain.usecase.Authenticate;
+import com.ms.user.presentation.protocol.Controller;
+
+@RestController
+public class AuthenticationController implements Controller<AuthenticationDto, ResponseEntity<Object>> {
+
+	public AuthenticationController(Authenticate authenticate) {
+		this.authenticate = authenticate;
+	}
+	
+	private Authenticate authenticate;
+    
+	@RequestMapping(value = "/user/authentication", method = RequestMethod.POST)
+	public ResponseEntity<Object> handle(@RequestBody AuthenticationDto authenticationDto) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		String errorMessage = authenticationDto.getError();
+		if (errorMessage != null) {
+			response.put("error", errorMessage);
+			return new ResponseEntity<Object>(response, HttpStatus.BAD_REQUEST);
+		}
+		DefaultReturn<String> result = this.authenticate.auth(
+			authenticationDto.getEmail(),
+			authenticationDto.getPassword()
+		);
+        String token = result.getContent();
+		response.put("token", token);
+		return new ResponseEntity<Object>(response, HttpStatus.CREATED);
+	}
+}
