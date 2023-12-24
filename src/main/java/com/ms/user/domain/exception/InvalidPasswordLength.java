13653779f1a1f7e4a6dd51cb4cace4e21da4561c
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 11:18:18 -0300

    feat: set InvalidPasswordLength message to improve log readability

--- src/main/java/com/ms/user/domain/exception/InvalidPasswordLength.java
+++ src/main/java/com/ms/user/domain/exception/InvalidPasswordLength.java
@@ -3,7 +3,7 @@ package com.ms.user.domain.exception;
 public class InvalidPasswordLength extends Exception {
 
     public InvalidPasswordLength () throws Exception {
-        throw new Exception("");
+        throw new Exception("Password have length less than 10. Please, use a password with more than 10 caracteres.");
     }
 
 }
