ef72a97879fe917aa7b13aa8eeb8d9ba7efaa514
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 18:43:11 -0300

    feat: BCryptPasswordAdapter implements ComparePassword and should be able to compare passwors when necessary

--- src/main/java/com/ms/user/infra/cryptography/BCryptPasswordAdapter.java
+++ src/main/java/com/ms/user/infra/cryptography/BCryptPasswordAdapter.java
@@ -3,11 +3,13 @@ package com.ms.user.infra.cryptography;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.ms.user.data.protocol.ComparePassword;
 import com.ms.user.data.protocol.Hasher;
 import com.ms.user.domain.model.Hash;
+import com.ms.user.domain.model.Password;
 
 @Component
-public class BCryptPasswordAdapter implements Hasher {
+public class BCryptPasswordAdapter implements Hasher, ComparePassword {
 
     @Override
     public Hash hash(String plaintext) {
@@ -16,4 +18,10 @@ public class BCryptPasswordAdapter implements Hasher {
         return new Hash(hashString);
     }
 
+    @Override
+    public boolean compare(Password password, Password passwordEncoded) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        boolean isValidPassword = encoder.matches(password.getValue(), passwordEncoded.getValue());
+        return isValidPassword;
+    }
 }
