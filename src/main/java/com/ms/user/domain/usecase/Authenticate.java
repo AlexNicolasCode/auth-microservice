e85404a52d5c72faa8affa1d6ea96e74b253f699
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 09:50:43 -0300

    feat: apply DefaultReturn as return on Authenticate usecase to enable return an error too on return when necessary

--- src/main/java/com/ms/user/domain/usecase/Authenticate.java
+++ src/main/java/com/ms/user/domain/usecase/Authenticate.java
@@ -1,8 +1,9 @@
 package com.ms.user.domain.usecase;
 
+import com.ms.user.domain.model.DefaultReturn;
 import com.ms.user.domain.model.Email;
 import com.ms.user.domain.model.Password;
 
 public interface Authenticate {
-    String auth(Email email, Password password);
+    DefaultReturn<String> auth(Email email, Password password);
 }
