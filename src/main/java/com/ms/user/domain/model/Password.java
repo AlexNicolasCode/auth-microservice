d8384e0ba65489ef72d1e4f2c89094cb410360f4
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 11:20:15 -0300

    fix: change throw erro to correct exception to avoid send wrong alert

--- src/main/java/com/ms/user/domain/model/Password.java
+++ src/main/java/com/ms/user/domain/model/Password.java
@@ -1,13 +1,13 @@
 package com.ms.user.domain.model;
 
-import com.ms.user.domain.exception.InvalidEmailException;
+import com.ms.user.domain.exception.InvalidPasswordLength;
 
 public final class Password {
     private String value;
 
-    public Password(String value) throws InvalidEmailException, Exception {
+    public Password(String value) throws InvalidPasswordLength, Exception {
         if (value.length() < 10){
-            throw new InvalidEmailException();
+            throw new InvalidPasswordLength();
         }
         this.value = value;
     }
