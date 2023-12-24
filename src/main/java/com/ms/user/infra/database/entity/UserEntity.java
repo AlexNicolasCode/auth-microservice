03119dc1dfb63bbd3136817d9dcd23d1c436056d
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 18:41:34 -0300

    feat: change name field to be nullable when avoid throw system when client side not send name field

--- src/main/java/com/ms/user/infra/database/entity/UserEntity.java
+++ src/main/java/com/ms/user/infra/database/entity/UserEntity.java
@@ -9,7 +9,7 @@ public class UserEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
 	private String name;
 	
     @Column(nullable = false)
