244aaba6c39338350dc88037fc3bc01f6157a5f2
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 09:33:00 -0300

    feat: InvalidPasswordLength Expection was created to enable throw process when password is invalid

---
+++ src/main/java/com/ms/user/domain/exception/InvalidPasswordLength.java
@@ -0,0 +1,9 @@
+package com.ms.user.domain.exception;
+
+public class InvalidPasswordLength extends Exception {
+
+    public InvalidPasswordLength () throws Exception {
+        throw new Exception("");
+    }
+
+}
