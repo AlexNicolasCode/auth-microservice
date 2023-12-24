cf7c29d08d5377fdf6cff84cd31de690ea735c3e
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 09:52:36 -0300

    feat: DbAuthentication apply Authenticate interface usecase and should be able to authenticate use

---
+++ src/main/java/com/ms/user/data/usecase/DbAuthentication.java
@@ -0,0 +1,45 @@
+package com.ms.user.data.usecase;
+
+import com.ms.user.data.protocol.ComparePassword;
+import com.ms.user.data.protocol.GenerateToken;
+import com.ms.user.data.protocol.GetKeys;
+import com.ms.user.data.protocol.LoadUserByEmailRepository;
+import com.ms.user.domain.model.DefaultReturn;
+import com.ms.user.domain.model.Email;
+import com.ms.user.domain.model.Password;
+import com.ms.user.domain.model.User;
+import com.ms.user.domain.usecase.Authenticate;
+
+public class DbAuthentication implements Authenticate {
+    private final LoadUserByEmailRepository loadUserByEmailRepository;
+    private final ComparePassword comparePassword;
+    private final GenerateToken generateToken;
+    private final GetKeys getKeys;
+
+    public DbAuthentication (
+        LoadUserByEmailRepository loadUserByEmailRepository,
+        ComparePassword comparePassword,
+        GenerateToken generateToken,
+        GetKeys getKeys
+    ) {
+        this.loadUserByEmailRepository = loadUserByEmailRepository;
+        this.comparePassword = comparePassword;
+        this.generateToken = generateToken;
+        this.getKeys = getKeys;
+    }
+
+    @Override
+    public DefaultReturn<String> auth(Email email, Password password) {
+        User user = this.loadUserByEmailRepository.loadUserByEmail(email);
+        boolean isValidPassword = this.comparePassword.compare(password, user.getPassword());
+        if (!isValidPassword) {
+            return new DefaultReturn<String>("Invalid Password", null);
+        }
+        String token = this.generateToken.generateToken(
+            user.getId(),
+            this.getKeys.getPublicKey(),
+            this.getKeys.getPrivateKey()
+        );
+        return new DefaultReturn<String>(null, token);
+    }
+}
