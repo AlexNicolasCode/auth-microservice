bb87ea4a8bf9f041c69f3316512dcfe0e75e935e
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-24 18:46:07 -0300

    feat: add getByEmail to enable get user using email as param and use UserEntity content

--- src/main/java/com/ms/user/infra/database/repository/UserSpringRepository.java
+++ src/main/java/com/ms/user/infra/database/repository/UserSpringRepository.java
@@ -10,6 +10,9 @@ import com.ms.user.infra.database.entity.UserEntity;
 public interface UserSpringRepository extends CrudRepository<UserEntity, Long> {
     @Query(value = "SELECT u FROM tb_user u WHERE u.token = ?1", nativeQuery = true)
     UserEntity getByAccessToken(String accessToken);
+
+    @Query(value = "SELECT * FROM tb_user WHERE email = ?1", nativeQuery = true)
+    UserEntity getByEmail(String email);
     
     @Query(value = "UPDATE tb_user u SET token = ?2 WHERE u.email = ?1 RETURNING u.token", nativeQuery = true)
     String updateToken(String email, String token);
