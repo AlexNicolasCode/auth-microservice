8f06b0911ccfb69e1bea11501dac7abeac61e652
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-25 11:55:37 -0300

    refactor: move BeanConfiguration from infra to main layer to improve architecture redability

--- src/main/java/com/ms/user/main/config/BeanConfiguration.java
+++ src/main/java/com/ms/user/main/config/BeanConfiguration.java
@@ -1,4 +1,4 @@
-package com.ms.user.infra.configuration;
+package com.ms.user.main.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
